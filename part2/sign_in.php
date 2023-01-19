9e06603dd8d6b6f5d298b6e01bd1dc2bd3c7c4b8
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-01-19 15:22:41 -0700

    Working on part 2, thank God

---
+++ part2/sign_in.php
@@ -0,0 +1,92 @@
+<?php
+    session_start();
+    require_once('db_connection.php');
+
+?>
+<html>
+	<head>
+		<title>Sign-In/Sign-Up</title>
+		<link rel="stylesheet" type="text/css" href="../shared/style.css">
+		<meta charset="utf-8">
+	</head>
+	
+	<body>
+        <div class = "body">
+            <h1>Menu</h1>
+            <nav>
+                <ul>
+                    <li><a href="../tma.htm">Home</a></li>
+                    <li><a href="index.php">Part 1</a></li>
+                    <li><a href="part2/part2.html">Part 2</a></li>
+                    <li><a href="part3/part3.html">Part 3</a></li>
+                    <li><a href="part4/part4.html">Part 4</a></li>
+                </ul>
+            </nav>
+			<div>	
+            <h3><a href="../tma.htm">Bookmarking App</a></h3>
+
+            <form action = "sign_in.php" method= "POST">
+                <!--form for inputs for login-->
+                <input type ="email" name = "email" placeholder ="Enter Email" required><br><br>
+                <input type ="text" name = "pass" placeholder ="Enter Password" required><br><br>
+                <button type ="submit" name="submit">Sign-In</button>
+            </form>
+
+<?php 
+
+
+    $conn = setup_database();
+
+    if(isset($_POST['submit'])){
+        
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+
+        $query = "SELECT * FROM users WHERE email = '$email' AND password = '$pass';";
+
+        $result = mysqli_query($conn, $query);
+
+        if(mysqli_num_rows($result) == 0){
+
+            echo " Wrong passowrd or email ";
+
+        }
+        else{
+            while($row = mysqli_fetch_array($result)) {
+                echo $row['id']; // Print a single column data
+                $_SESSION['id'] = $row['id'];
+            }
+
+            header("Location:home.php");
+            exit();
+        }
+    }
+    
+
+    
+
+    // else{
+    //     $query = "INSERT INTO users(id, name, email, password) VALUES($id, '$name', '$email', '$pass');";
+
+
+    //     while(mysqli_query($conn, $query)==false){
+    //         $id = rand(1, 1000);
+
+    //         $query = "INSERT INTO users VALUES($id, $name, $email, $pass);";
+    //     }
+
+    //     echo " Sign-Up successful ";
+    // }
+    // $email = '';
+    // $pass = '';
+    // $name = '';
+
+
+
+    close_connection($conn);
+?>
+
+<p><strong>Don't have an account?</strong></p><a href="sign_up.php">Sign-Up</a>
+</div>
+
+</html>
---
+++ part2/sign_up.php
@@ -0,0 +1,85 @@
+<?php
+    session_start();
+
+    require_once('db_connection.php');
+
+?>
+<html>
+	<head>
+		<title>Sign-In/Sign-Up</title>
+		<link rel="stylesheet" type="text/css" href="../shared/style.css">
+		<meta charset="utf-8">
+	</head>
+	
+	<body>
+        <div class = "body">
+            <h1>Menu</h1>
+            <nav>
+                <ul>
+                    <li><a href="../tma.htm">Home</a></li>
+                    <li><a href="index.php">Part 1</a></li>
+                    <li><a href="part2/part2.html">Part 2</a></li>
+                    <li><a href="part3/part3.html">Part 3</a></li>
+                    <li><a href="part4/part4.html">Part 4</a></li>
+                </ul>
+            </nav>
+			<div>	
+            <h3><a href="../tma.htm">Bookmarking App</a></h3>
+
+            <form action = "sign_up.php" method= "POST">
+                    <!--form for inputs for login-->
+                    <input type ="text" name = "name" placeholder ="Enter Name" required><br><br>
+                    <input type ="email" name = "email" placeholder ="Enter Email" required><br><br>
+                    <input type ="text" name = "pass" placeholder ="Enter Password" required><br><br>
+                    <button type ="submit" name="submit">Sign-Up</button>
+            </form>
+
+<?php 
+
+
+
+    $conn = setup_database();
+
+    if(isset($_POST['submit'])){
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+        $name = $_POST['name'];
+
+        $id = rand(1, 10000);
+
+        $query = "SELECT * FROM users WHERE email = '$email';";
+
+        $result = mysqli_query($conn, $query);
+
+        // checking if the email already exists
+        if(mysqli_num_rows($result) > 0){
+
+            echo " This email is already used! \n";
+
+        }
+
+        // create the account
+        else{
+            $query = "INSERT INTO users(id, name, email, password) VALUES($id, '$name', '$email', '$pass');";
+
+            // if id has already been used
+            while(mysqli_query($conn, $query)==false){
+                $id = rand(1, 10000);
+
+                $query = "INSERT INTO users VALUES($id, '$name', '$email', '$pass');";
+            }
+
+            // taking the user to the home page
+            $_SESSION['id'] = $id;
+            header("Location:home.php");
+            exit();
+        }
+    }
+    
+    close_connection($conn);
+?>
+
+<p><strong>Already have an account?</strong></p><a href="sign_in.php">Sign-In</a>
+</div>
+
+</html>
